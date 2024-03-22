65af307ebf5ae62c54ff4843963c1ecd4146bd12
Maggie <guachachi02@yahoo.es>
2024-03-22 21:32:32 +0100

    Añadir componente Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <header className="bg-black border-b py-20 px-20">
+            <img src="/EthDefiLogo.jpeg" alt="EthDefiLogo-mobile-logo" width={600} />
+        </header>
+    )
+}
+
+
