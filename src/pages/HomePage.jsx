cd37a0331fda8d65c172cd9ac6073cbf8514bffe
Maggie <guachachi02@yahoo.es>
2024-03-22 23:42:41 +0100

    Añadir componente pagina HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div className="flex flex-col place-items-center gap-1">
+          <h1 className="text-7xl font-arial py-20">Préstamos Finanzas Descentralizados</h1>
+          <button className="text-3xl">🔒 Connect Wallet...</button>
+        </div>
+    )
+}
