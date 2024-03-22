956d9640592e76020ce93399a5f2b3713dfb7b7b
Maggie <guachachi02@yahoo.es>
2024-03-22 21:44:59 +0100

    Actualizar componente App con AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { AppLayout } from "./components/ui/layouts"
+
 function App() {
-  return <h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1>
+  return (
+    <>
+      <AppLayout>
+        <div className="py-20 flex justify-center">
+          <h1 className="text-7xl font-arial">Préstamos Finanzas Descentralizados</h1>
+        </div>
+        <div className="items-center flex justify-center">
+          <button className="text-3xl">🔒 Connect Wallet...</button>
+        </div>
+      </AppLayout>
+    </>
+  )
 }
 
 export default App
